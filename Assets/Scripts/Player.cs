b47608f308bcf93d5bec1cb8079844b79c0184c3
Zuko <zuko@MacBook-Pro.local>
2020-06-03 15:51:48 -0700

    player running animation while running, idle while idle

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,22 +5,29 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
-
+    //Config
     [SerializeField] float runSpeed = 5f;
 
+    //State
+    bool isAlive = true;
 
+    //Cached component references
     Rigidbody2D myRigidBody;
+    Animator myAnimator;
 
+    //Message then methods
     // Start is called before the first frame update
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
         Run();
+        FlipSprite();
     }
 
     private void Run()
@@ -28,5 +35,19 @@ public class Player : MonoBehaviour
         float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal");  // value is between -1 to +1
         Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
         myRigidBody.velocity = playerVelocity;
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("Running", playerHasHorizontalSpeed);
+    }
+
+    private void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        // if the player is moving horizontally
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x), 1f);
+            // reverse the current scaling of x axis
+        }
     }
 }
