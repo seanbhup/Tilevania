441288bac2db21f02c3bd4c6c4ce52139b23acd5
Zuko <zuko@MacBook-Pro.local>
2020-06-03 16:20:30 -0700

    add jumping method, adjust jump speed and gravity, implement single jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,7 @@ public class Player : MonoBehaviour
 {
     //Config
     [SerializeField] float runSpeed = 5f;
+    [SerializeField] float jumpSpeed = 5f;
 
     //State
     bool isAlive = true;
@@ -14,13 +15,16 @@ public class Player : MonoBehaviour
     //Cached component references
     Rigidbody2D myRigidBody;
     Animator myAnimator;
+    Collider2D myCollider2D;
 
     //Message then methods
     // Start is called before the first frame update
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
-        myAnimator = GetComponent<Animator>(); 
+        myAnimator = GetComponent<Animator>();
+        myCollider2D = GetComponent<Collider2D>(); 
+        
     }
 
     // Update is called once per frame
@@ -28,6 +32,7 @@ public class Player : MonoBehaviour
     {
         Run();
         FlipSprite();
+        Jump();
     }
 
     private void Run()
@@ -35,9 +40,24 @@ public class Player : MonoBehaviour
         float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal");  // value is between -1 to +1
         Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
         myRigidBody.velocity = playerVelocity;
+        //print(playerVelocity);
 
         bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
         myAnimator.SetBool("Running", playerHasHorizontalSpeed);
+    }
+
+    private void Jump()
+    {
+        if (!myCollider2D.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            return;
+        }
+
+        if (CrossPlatformInputManager.GetButtonDown("Jump"))
+        {
+            Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeed);
+            myRigidBody.velocity += jumpVelocityToAdd;
+        }
     }
 
     private void FlipSprite()
